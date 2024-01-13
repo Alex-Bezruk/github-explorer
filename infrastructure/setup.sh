5b70b684d0cfe9e1e577731bf860bd3ec59e251d
Alex Bezruk <oleksandr.bezruk@gmail.com>
2024-01-13 23:55:34 +0200

    Configure AWS infrastructure

--- infrastructure/setup.sh
+++ infrastructure/setup.sh
@@ -4,15 +4,11 @@
 AWS_REGION="eu-west-1"
 
 # Set your CloudFormation stack name
-STACK_NAME="GithubExplorerStack"
+STACK_NAME="GithubExplorerStackTest"
 
 # AWS CLI command to deploy the CloudFormation stack
 aws cloudformation create-stack \
   --stack-name $STACK_NAME \
-  --template-body file://cloudformation.yml  \
-  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+  --template-body file://template.yml  \
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
   --region $AWS_REGION
-
-# Wait for the stack to complete
-echo "Waiting for the stack to complete..."
-aws cloudformation wait stack-create-complete --stack-name $STACK_NAME --region $AWS_REGION
